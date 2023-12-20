d5d881bf41ccb4cb456f01d23d1a05da86f4d800
CBL_MAC_21 <honeykumar@codebrewinnovations.com>
2023-12-20 15:14:12 +0530

    fix

---
+++ Self Learning/variables.java
@@ -0,0 +1,25 @@
+class Scratch {
+    public static void main(String[] args) {
+        String myString = "This is string";
+
+        int num = 10;
+
+        double doubleNum = 98.64;
+
+        boolean isBoolean = true;
+
+        System.out.println(myString);
+        System.out.println(num);
+        System.out.println(doubleNum);
+        System.out.println(isBoolean);
+
+        int a = 10;
+        double b = 7.6;
+        double c = 0.4;
+
+        double result = a + b + c;
+
+        System.out.println(result);
+
+    }
+}
