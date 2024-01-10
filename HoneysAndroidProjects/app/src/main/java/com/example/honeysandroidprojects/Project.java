8be8a7170c4cfbc0851d51025c32396ec54446e6
CBL_MAC_21 <honeykumar@codebrewinnovations.com>
2024-01-10 20:39:47 +0530

    fix

---
+++ HoneysAndroidProjects/app/src/main/java/com/example/honeysandroidprojects/Project.java
@@ -0,0 +1,13 @@
+package com.example.honeysandroidprojects;
+
+public class Project {
+    String name;
+    String description;
+    int image;
+
+    Project(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+}
---
+++ HoneysAndroidProjects/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Honey's Android Projects"
+include(":app")
